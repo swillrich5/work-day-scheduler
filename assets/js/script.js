a499f27e3729c5704263ae905b4eba3485b1b4e2
swillrich5 <swillrich@gmail.com>
2021-03-31 18:49:45 -0500

    table backgrounds synched with time of day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,33 @@
 var currentDay = document.querySelector("#currentDay");
+var timeColumns = document.querySelectorAll(".time-block")
 
 
 
+function setTimeColumnBackground(rightNow) {
+    for (var i=0; i < timeColumns.length; i++) {
+        if (moment("11am", "hA").format("H") == (i+9)) {
+            timeColumns[i].classList.remove("past");
+            timeColumns[i].classList.remove("future");
+            timeColumns[i].classList.add("present");
+        } else if (moment("11am", "hA").format("H") > (i+9)) {
+            timeColumns[i].classList.remove("present");
+            timeColumns[i].classList.remove("future");
+            timeColumns[i].classList.add("past");
+        } else if (moment("11am", "hA").format("H") < (i+9)) {
+            timeColumns[i].classList.remove("present");
+            timeColumns[i].classList.remove("past");
+            timeColumns[i].classList.add("future");
+        }
+    }
 
+}
 
 
 // handle displaying the time
 function displayTime() {
     var rightNow = moment().format('MMM DD, YYYY');
     currentDay.textContent = rightNow;
+    setTimeColumnBackground();
   }
 
 
