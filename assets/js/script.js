0aae8e6d035404dac03011e6358b6e0047be8f9e
swillrich5 <swillrich@gmail.com>
2021-04-01 00:12:19 -0500

    tweaking layout bootstrap and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,26 @@
 var currentDay = document.querySelector("#currentDay");
-var timeColumns = document.querySelectorAll(".time-block")
+var timeColumns = document.querySelectorAll(".data-entry");
+var inputField = document.querySelectorAll(".input-field");
 
 
 
 function setTimeColumnBackground(rightNow) {
+    var currentHour = moment("12pm", "hA").format("H");
+    console.log(currentHour);
     for (var i=0; i < timeColumns.length; i++) {
-        if (moment("11am", "hA").format("H") == (i+9)) {
+        console.log("currentHour = " + currentHour + " i+9 = " + (i+9) + " i = " + i);
+        console.log((currentHour == (i+9)));
+        if (currentHour == (i+9)) {  // present hour
             timeColumns[i].classList.remove("past");
             timeColumns[i].classList.remove("future");
             timeColumns[i].classList.add("present");
-        } else if (moment("11am", "hA").format("H") > (i+9)) {
+
+        } else if (currentHour > (i+9)) {  // past hours
+            // console.log("before noon");
             timeColumns[i].classList.remove("present");
             timeColumns[i].classList.remove("future");
             timeColumns[i].classList.add("past");
-        } else if (moment("11am", "hA").format("H") < (i+9)) {
+        } else if (currentHour < (i+9)) {  // future hours
             timeColumns[i].classList.remove("present");
             timeColumns[i].classList.remove("past");
             timeColumns[i].classList.add("future");
@@ -27,7 +34,7 @@ function setTimeColumnBackground(rightNow) {
 function displayTime() {
     var rightNow = moment().format('MMM DD, YYYY');
     currentDay.textContent = rightNow;
-    setTimeColumnBackground();
+    setTimeColumnBackground(rightNow);
   }
 
 
